4337b4e173429649febc402c49ca4019cd1bc262
Daniel <daniloviciv@hotmail.com>
2018-12-16 02:10:04 +0100

    employee form to create new employees

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Employee;
+use App\Form\EmployeeType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function admin()
+    {
+        return $this->render('admin/index.html.twig', [
+        ]);
+    }
+
+    /**
+     * @Route("/admin/create-employee", name="admin_create_employee")
+     */
+    public function createEmployee(Request $request) : Response
+    {
+        $employee = new Employee();
+
+        $form = $this->createForm(EmployeeType::class, $employee);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($employee);
+            $em->flush();
+
+            $this->addFlash('success', 'employee.created_successfully');
+
+            return $this->redirectToRoute('team');
+        }
+        return $this->render('admin/create_employee.html.twig', [
+            'employee' => $employee,
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Form/EmployeeType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+use App\Entity\Employee;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+
+class EmployeeType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        // For the full reference of options defined by each form field type
+        // see https://symfony.com/doc/current/reference/forms/types.html
+        // By default, form fields include the 'required' attribute, which enables
+        // the client-side form validation. This means that you can't test the
+        // server-side validation errors from the browser. To temporarily disable
+        // this validation, set the 'required' attribute to 'false':
+        // $builder->add('title', null, ['required' => false, ...]);
+        $builder
+            ->add('name', TextType::class, [
+                'attr' => ['autofocus' => true],
+                'label' => 'label.name',
+            ])
+            ->add('email', TextType::class, [
+                'label' => 'label.email',
+            ])
+            ->add('incorporationDate', DateTimeType::class, [
+                'label' => 'label.incorporationDate',
+                'help' => 'help.incorporationDate',
+            ])
+        ;
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Employee::class,
+        ]);
+    }
+}
