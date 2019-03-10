04996eae80510240ba54404ea6e51ab49f81e901
Daniel <daniloviciv@hotmail.com>
2019-03-10 22:56:50 +0100

    change translatorInterface deprecation by the new interface

--- src/EventSubscriber/ProfileNotificationSubscriber.php
+++ src/EventSubscriber/ProfileNotificationSubscriber.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the Symfony package.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace App\EventSubscriber;
 
 use App\Entity\Employee;
@@ -17,13 +8,8 @@ use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\EventDispatcher\GenericEvent;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Component\Translation\TranslatorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
-/**
- * Notifies post's author about new comments.
- *
- * @author Oleg Voronkovich <oleg-voronkovich@yandex.ru>
- */
 class ProfileNotificationSubscriber implements EventSubscriberInterface
 {
     private $mailer;
